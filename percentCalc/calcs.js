df31d37f50827c44ca1561ee425413255fbb1e7d
jpPlum <40789735+umekami@users.noreply.github.com>
2019-01-17 16:28:35 +0900

    Add files via upload

--- percentCalc/calcs.js
+++ percentCalc/calcs.js
@@ -1,19 +1,21 @@
 function getValue() {
-	valueA = parseInt(document.forms.abform.atext.value);
-	valueB = parseInt(document.forms.abform.btext.value);
+	answerId = document.getElementById('answer');
+	valueA = parseInt(document.forms.abform.atext.value, 10);
+	valueB = parseInt(document.forms.abform.btext.value, 10);
 };
-
 
 //指定数値のx%分の値を求める
 function perValue() {
 	getValue();
-	console.log(valueB * valueA / 100);
+	let answer = valueB * valueA / 100;
+	answerId.innerHTML = "答え:" + answer;
 };
 
 //パーセントを求める
 function findPercent() {
 	getValue();
-	console.log(valueB / valueA * 100 + "%");
+	let answer = valueB / valueA * 100 + "%";
+	answerId.innerHTML = "答え:" + answer;
 };
 
 //指定数値に対してx%分足す＆引く
@@ -21,7 +23,8 @@ function perCalcPlusMinus() {
 	getValue();
 	var floatpercent = valueA / 100;
 	var addvalue = valueB * floatpercent;
-	console.log(valueB + "%分足すと:" + (valueA + addvalue));
-	console.log(valueB + "%分引くと:" + (valueA - addvalue));
-}
 
+	let plusAnswer = valueB + "%分足すと:" + (valueA + addvalue);
+	let minusAnswer = valueB + "%分引くと:" + (valueA - addvalue);
+	answerId.innerHTML = "答え:" + plusAnswer + " " + minusAnswer;
+}
